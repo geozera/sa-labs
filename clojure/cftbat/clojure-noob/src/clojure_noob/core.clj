53723155a8dd1bd392343d2e4e6167ade939dfdc
Geovany Bezerra <geovany.bezerra@modernatec.com.br>
2026-09-06 15:55:58 -0300

    syntax practice

--- clojure/cftbat/clojure-noob/src/clojure_noob/core.clj
+++ clojure/cftbat/clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,33 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "I'm a little teapot!"))
+
+  []
+
+  (defn matching-part
+    [body-part]
+
+    {:name (str/replace (:name body-part) #"^left-" "right-")
+     :size (:size body-part)})
+
+  (defn symmetrize-body
+    [unsym-body]
+    (loop [remaining-body-parts unsym-body final-body-parts []]
+      (if (empty? remaining-body-parts)
+        final-body-parts
+        (let [[part & remaining] remaining-body-parts]
+          (recur remaining
+                 (into final-body-parts
+                       (set[part (matching-part part)])))))))
+
+  (println (symmetrize-body [{:name "head" :size 3}
+                             {:name "nose" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "eyes" :size 1}
+                             {:name "left-arm" :size 5}
+                             {:name "left-leg" :size 5}])))
+
